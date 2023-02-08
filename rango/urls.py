1ce615b5c6c129600ecf5aee3d58e8a2fd00a94a
Pieter Van Tuijl <pietervantuijl2000@gmail.com>
2023-02-08 18:15:16 +0000

    main: create initial url mapping

---
+++ rango/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rango import views
+
+# together with the namespace in the template, this allows us to use {% url 'rango:index' %} in the template
+app_name = 'rango'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
