cd15f4789aece752047613896f9948c0cd38b731
Pieter Van Tuijl <pietervantuijl2000@gmail.com>
2023-02-10 12:47:48 +0000

    main: add media/static content + use templates

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    return render(request, 'rango/about.html')
